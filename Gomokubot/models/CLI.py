adbc0d54fb20fc23736bbf61958cfc52c7d3f2d0
Alexander Stone <aloecend@gmail.com>
2018-09-10 15:49:50 -0700

    commented out models.

--- Gomokubot/models/CLI.py
+++ Gomokubot/models/CLI.py
@@ -1,112 +1,112 @@
-import sys
-import uuid
-from textwrap import dedent
-try:
-    from .gmk_board import Board
-except ModuleNotFoundError:
-    from gmk_board import Board
+# import sys
+# import uuid
+# from textwrap import dedent
+# try:
+#     from .gmk_board import Board
+# except ModuleNotFoundError:
+#     from gmk_board import Board
 
-# stone placement verification
-
-
-bd = Board()
+# # stone placement verification
 
 
-bd.stone = 1
+# bd = Board()
 
 
-bd.place_piece(7, 7)
+# bd.stone = 1
 
 
-bd.place_piece(7, 9)
+# bd.place_piece(7, 7)
 
 
-bd.place_piece(8, 7)
+# bd.place_piece(7, 9)
 
 
-bd.stone = 2
+# bd.place_piece(8, 7)
 
 
-bd.place_piece(7, 8)
+# bd.stone = 2
 
 
-bd.place_piece(8, 8)
+# bd.place_piece(7, 8)
 
 
-bd.place_piece(9, 7)
+# bd.place_piece(8, 8)
 
 
-def draw_board_row(line):
-    """
-    evaluates the board class and draws stone locations
-    (<four monospace>)
-    """
-    row = ''
-    for place in line:
-        if place == 0:
-            row += (' .  ')
-        if place == 1:
-            row += (' X  ')
-        if place == 2:
-            row += (' O  ')
-    return row
+# bd.place_piece(9, 7)
 
 
-def display_board():
-    """
-    board spaced for stone placement
-    """
-    print(f'''
-             00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14
-             ----------------------------------------------------------
-        00 | {draw_board_row(bd.board[0])}
-
-        01 | {draw_board_row(bd.board[1])}
-
-        02 | {draw_board_row(bd.board[2])}
-
-        03 | {draw_board_row(bd.board[3])}
-
-        04 | {draw_board_row(bd.board[4])}
-
-        05 | {draw_board_row(bd.board[5])}
-
-        06 | {draw_board_row(bd.board[6])}
-
-        07 | {draw_board_row(bd.board[7])}
-
-        08 | {draw_board_row(bd.board[8])}
-
-        09 | {draw_board_row(bd.board[9])}
-
-        10 | {draw_board_row(bd.board[10])}
-
-        11 | {draw_board_row(bd.board[11])}
-
-        12 | {draw_board_row(bd.board[12])}
-
-        13 | {draw_board_row(bd.board[13])}
-
-        14 | {draw_board_row(bd.board[14])}
-    ''')
+# def draw_board_row(line):
+#     """
+#     evaluates the board class and draws stone locations
+#     (<four monospace>)
+#     """
+#     row = ''
+#     for place in line:
+#         if place == 0:
+#             row += (' .  ')
+#         if place == 1:
+#             row += (' X  ')
+#         if place == 2:
+#             row += (' O  ')
+#     return row
 
 
-def exit():
-    '''
-    exits the app
-    '''
-    print(dedent('''
-        EXIT PHRASE
-    '''))
-    sys.exit()
+# def display_board():
+#     """
+#     board spaced for stone placement
+#     """
+#     print(f'''
+#              00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14
+#              ----------------------------------------------------------
+#         00 | {draw_board_row(bd.board[0])}
+
+#         01 | {draw_board_row(bd.board[1])}
+
+#         02 | {draw_board_row(bd.board[2])}
+
+#         03 | {draw_board_row(bd.board[3])}
+
+#         04 | {draw_board_row(bd.board[4])}
+
+#         05 | {draw_board_row(bd.board[5])}
+
+#         06 | {draw_board_row(bd.board[6])}
+
+#         07 | {draw_board_row(bd.board[7])}
+
+#         08 | {draw_board_row(bd.board[8])}
+
+#         09 | {draw_board_row(bd.board[9])}
+
+#         10 | {draw_board_row(bd.board[10])}
+
+#         11 | {draw_board_row(bd.board[11])}
+
+#         12 | {draw_board_row(bd.board[12])}
+
+#         13 | {draw_board_row(bd.board[13])}
+
+#         14 | {draw_board_row(bd.board[14])}
+#     ''')
 
 
-if __name__ == '__main__':
-    '''
-    run : core
-    key exit : polite
-    '''
-    try:
-        display_board()
-    except KeyboardInterrupt:
-        exit()
+# def exit():
+#     '''
+#     exits the app
+#     '''
+#     print(dedent('''
+#         EXIT PHRASE
+#     '''))
+#     sys.exit()
+
+
+# if __name__ == '__main__':
+#     '''
+#     run : core
+#     key exit : polite
+#     '''
+#     try:
+#         display_board()
+#     except KeyboardInterrupt:
+#         exit()
--- Gomokubot/models/p_hum.py
+++ Gomokubot/models/p_hum.py
@@ -1,15 +1,15 @@
-class hum(object):
-    """
-    human. serves as function nexus between logician and board class
-    """
-    def __init__(self):
-        self.id = 0
+# class hum(object):
+#     """
+#     human. serves as function nexus between logician and board class
+#     """
+#     def __init__(self):
+#         self.id = 0
 
-    # sees board
+#     # sees board
 
-    # time to think
+#     # time to think
 
-    # makes decision
+#     # makes decision
 
-    # calls board methods for decision making through interface
-        # board class executes rest cycle
+#     # calls board methods for decision making through interface
+#         # board class executes rest cycle
