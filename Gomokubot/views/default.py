f9ee5c70d29064160c3489049cb209d613795f63
Alexander Stone <aloecend@gmail.com>
2018-09-10 15:53:25 -0700

    changed default message to be one line.

--- Gomokubot/views/default.py
+++ Gomokubot/views/default.py
@@ -3,5 +3,4 @@ from pyramid.response import Response
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def home_view(request):
-    message = 'hello world'
-    return Response(body=message, content_type='application/json', status=200)
+    return Response(body="Hello World", content_type='application/json', status=200)
