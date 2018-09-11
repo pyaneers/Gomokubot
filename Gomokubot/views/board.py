9dc7db7e49560419ea19775e20cedface372f48d
Andrew Baik <andrewbaik@outlook.com>
2018-09-11 15:42:59 -0700

    API view implemented

--- Gomokubot/views/board.py
+++ Gomokubot/views/board.py
@@ -1,5 +1,13 @@
 from pyramid_restful.viewsets import APIViewSet
 from pyramid.response import Response
+from sqlalchemy.exc import DataError, IntegrityError
+from json.decoder import JSONDecodeError
+from sqlalchemy.exc import DBAPIError
+import requests
+import json
+from ..models.gmk_board import DBBoard
+from ..models.schema import DBBoardSchema
+from uuid import uuid4
 
 
 class BoardAPIView(APIViewSet):
@@ -7,29 +15,66 @@ class BoardAPIView(APIViewSet):
     """
     def create(self, request):
         """ Create a new Gomoku Game
+            POST api/v1/board/newgame
         """
-        return Response(json='Fake Create', status=201)
+        try:
+            kwargs = json.loads(request.body)
+        except JSONDecodeError as e:
+            return Response(json=e.msg, status=400)
 
-    def update(self, request, id=None):
+        try:
+            kwargs['uuid'] = uuid4.uuid()
+            board = DBBoard.new(request, **kwargs)
+        except IntegrityError:
+            return Response(json='uuid does not exist', status=400)
+
+        schema = DBBoardSchema()
+        data = schema.dump(board).data
+        return Response(json=data, status=201)
+
+    def update(self, request):
         """ Send a new move on a given game
+            PUT api/v1/board/**kwargs
         """
-        if not id:
-            return Response(json='Update Game Not Found', status=404)
-        mssg = f'Fake sent a move to game {id}'
-        return Response(json=mssg, status=200)
+        try:
+            kwargs = json.loads(request.body)
+        except JSONDecodeError as e:
+            return Response(json=e.msg, status=400)
+        try:
+            board = DBBoard.update(request=request, **kwargs)
+        except (DataError, AttributeError):
+            return Response(json='sent information is invalid', status=400)
 
-    def retrieve(self, request, id=None):
+        schema = DBBoardSchema()
+        data = schema.dump(board).data
+        return Response(json=data, status=200)
+
+    def retrieve(self, request):
         """ Get one Gomoku Game with given id
+            GET api/v1/board/**kwargs
         """
-        if not id:
-            return Response(json='Not Found', status=404)
-        mssg = f'Looking at game {id}'
-        # if move:
-        #     mssg += f' on move {move}'
+        try:
+            kwargs = json.loads(request.body)
+        except JSONDecodeError as e:
+            return Response(json=e.msg, status=400)
 
-        return Response(json=mssg, status=200)
+        try:
+            board = DBBoard.one(request=request, **kwargs)
+        except (DataError, AttributeError):
+            return Response(json='sent information is invalid', status=400)
+
+        schema = DBBoardSchema()
+        data = schema.dump(board).data
+        return Response(json=data, status=200)
 
     def list(self, request):
         """ List all current running games
+            GET api/v1/board
         """
-        return Response(json='Fake list active games', status=200)
+        if request is None:
+            return Response(json='Not Found', status=400)
+
+        all_records = DBBoard.all(request)
+        schema = DBBoardSchema()
+        data = [schema.dump(record).data for record in all_records]
+        return Response(json=data, status=200)
