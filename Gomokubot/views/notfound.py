a032fd0d087fa75f8c3d713c7cd60e2f7d36a160
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-10 23:35:39 -0700

    catch some file cleanup

--- Gomokubot/views/notfound.py
+++ Gomokubot/views/notfound.py
@@ -5,8 +5,8 @@ from pyramid.view import notfound_view_config
 @notfound_view_config(renderer='json')
 def notfound_view(request):
     request.response.status = 404
-    mssg = 'Not Found'
+    # mssg = 'Not Found'
     # mssg += request.json
-    request.response.json = 'Not Found'
-    return {}
+    # request.response.json = 'Not Found'
     # return Response(json=mssg, content_type="text/plain", status=404)
+    return {}
