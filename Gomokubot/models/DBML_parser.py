26279be9eceb9c76a004955bc4d17bd7eadb9b20
Jason Burns <jasonb315@protonmail.com>
2018-09-13 15:21:22 -0700

    ini

---
+++ Gomokubot/models/DBML_parser.py
@@ -0,0 +1,12 @@
+# write me a function that takes in a list of 25 elements
+# and returns a 2d array of 5 elements
+
+# and another in reverse
+
+
+def DBML_parser():
+    pass
+
+
+def MLDB_parser():
+    pass
